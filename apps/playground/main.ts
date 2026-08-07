4e1d3047b787bf7b364eb48b1a0930356da1cd43
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2026-08-07 19:14:49 -0300

    chore(playground): use a local SQLite database

--- apps/playground/main.ts
+++ apps/playground/main.ts
@@ -2,7 +2,18 @@ import { createKernel } from "@hyperkernel/kernel";
 import { Projects } from "./Projects.ts";
 import { Tasks } from "./Tasks.ts";
 
-const kernel = createKernel(Projects, Tasks);
+const kernel = createKernel({
+  config: {
+    database: {
+      path: "./local.db",
+    },
+  },
+  modules: {
+    Projects,
+    Tasks,
+  },
+});
 
 kernel.dispatch("CreateProject", { name: "Hyperkernel" });
+
 kernel.dispatch("CreateTask", { text: "Write documentation" });
